d4f45e6d02badbfc9807886358f2960f00dbc5ae
Marco Cavallo <mcavallo@welol.it>
2024-02-22 18:10:59 +0100

    Entra ID auth completed

--- CenturyBelongingCalculatorAPI/Controllers/WeatherForecastController.cs
+++ CenturyBelongingCalculatorAPI/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Identity.Web.Resource;
 using Microsoft.Graph;
 using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
 
 namespace CenturyBelongingCalculatorAPI.Controllers
 {
@@ -12,8 +13,6 @@ namespace CenturyBelongingCalculatorAPI.Controllers
     [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class WeatherForecastController : ControllerBase
     {
-        private readonly IDownstreamApi _downstreamApi;
-        private readonly GraphServiceClient _graphServiceClient;
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -21,29 +20,16 @@ namespace CenturyBelongingCalculatorAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, GraphServiceClient graphServiceClient, IDownstreamApi downstreamApi)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-            _graphServiceClient = graphServiceClient;
-            _downstreamApi = downstreamApi;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
+        [AuthorizeForScopes(Scopes = ["Forecast.Read"])]
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            using var response = await _downstreamApi.CallApiForUserAsync("DownstreamApi").ConfigureAwait(false);
-
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                var apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                // Do something
-            }
-            else
-            {
-                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
-            }
-            var user = await _graphServiceClient.Me.Request().GetAsync();
+            
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- CenturyBelongingCalculatorAPI/Program.cs
+++ CenturyBelongingCalculatorAPI/Program.cs
@@ -2,46 +2,61 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web;
+
 using Microsoft.Identity.Web.Resource;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Graph.ExternalConnectors;
+using Microsoft.Graph;
 
-namespace CenturyBelongingCalculatorAPI
+namespace CenturyBelongingCalculatorAPI;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options);
+                options.TokenValidationParameters.NameClaimType = "name";
+            }, 
+            options => { builder.Configuration.Bind("AzureAd", options); });
+            
+        builder.Services.AddAuthorization(config =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            config.AddPolicy("AuthZPolicy", policyBuilder =>
+                policyBuilder.Requirements.Add(new ScopeAuthorizationRequirement() { RequiredScopesConfigurationKey = $"AzureAd:Scopes" }));
+        });
 
-            // Add services to the container.
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        var app = builder.Build();
 
-            var app = builder.Build();
+        // Configure the HTTP request pipeline.
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI();
-            //}
-                app.UseSwagger();
-                app.UseSwaggerUI();
+        app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+        app.UseAuthentication();
 
-            app.UseAuthentication();
-
-            app.UseAuthorization();
+        app.UseAuthorization();
 
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }
